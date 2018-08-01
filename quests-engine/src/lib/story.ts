b7d7f5a6c56f5cbef498ba888b0089199eb1e839
Ali Waseem <waseema393@gmail.com>
2018-07-31 19:57:24 -0600

    refactored and added short id

--- quests-engine/src/lib/story.ts
+++ quests-engine/src/lib/story.ts
@@ -1,26 +1,26 @@
 import { Graph } from 'graphlib';
+import * as shortID from 'shortid';
+
+export function story(graph: Graph) {
+  return (text: string) => {
+    const ID = shortID.generate()
+    graph.setNode(ID, text)
+    return {
+      choice: choice(graph)(ID)
+    }
+  }
+}
 
 function choice(graph: Graph) {
-  return (parentStory: string) => {
-    return (id: string, text: string) => {
-      graph.setNode(id, text)
-      graph.setEdge(parentStory, id)
+  return (parentID: string) => {
+    return (text: string) => {
+      const ID = shortID.generate()
+      graph.setNode(ID, text)
+      graph.setEdge(parentID, ID)
       return {
-        choice: choice(graph)(parentStory)
+        choice: choice(graph)(parentID),
+        story: story(graph)
       }
     }
   }
 }
-
-function story(graph: Graph) {
-  return (id: string, text: string) => {
-    graph.setNode(id, text)
-    return {
-      choice: choice(graph)(id)
-    }
-  }
-}
-
-story()
-  .choice()
-  .choice()
