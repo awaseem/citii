c21c7979c2d6ae69395afa7e6ca56f0f499db0fb
Ali Waseem <waseema393@gmail.com>
2018-08-16 21:10:23 -0600

    updated spec to use only javascript objects

--- quests-engine/src/lib/story.spec.ts
+++ quests-engine/src/lib/story.spec.ts
@@ -1,29 +1,27 @@
 import test from 'ava';
-import { Graph } from 'graphlib';
-import { NodeTypes, story } from './story';
+import { story } from './story';
+
 
 test('creates story node with correct text and label', t => {
-  const { graph } = story(new Graph({ compound: true }))('this is a test');
+  const {storyNode} = story('this is a test')
 
-  t.is(graph.nodes().length, 1)
-  t.is(graph.node(NodeTypes.Starting), 'this is a test')
+  t.is(storyNode.text, 'this is a test')
 });
 
 test('creates a story with a single choice', t => {
-  const { graph } = story(new Graph({ compound: true }))('this is a test')
+  const {storyNode} = story('this is a test')
     .choice('this is a single choice')
 
-  t.is(graph.children(NodeTypes.Starting).length, 1)
-  t.is(graph.node(graph.children(NodeTypes.Starting)[0]), 'this is a single choice')
+  t.is(storyNode.choices.length, 1)
+  t.is(storyNode.choices[0].text, 'this is a single choice')
 })
 
 test('creates a story with a two choice', t => {
-  const { graph } = story(new Graph({ compound: true }))('this is a test')
-    .choice('this is choice 1')
-    .choice('this is choice 2')
+  const {storyNode} = story('this is a test')
+    .choice('this is a single choice')
+    .choice('this is the second choice')
 
-  t.is(graph.children(NodeTypes.Starting).length, 2)
-  graph.children(NodeTypes.Starting).forEach((nodeID, index) => {
-    t.is(graph.node(nodeID), `this is choice ${index + 1}`)    
-  })
+  t.is(storyNode.choices.length, 2)
+  t.is(storyNode.choices[0].text, 'this is a single choice')
+  t.is(storyNode.choices[1].text, 'this is the second choice')
 })
--- quests-engine/src/lib/story.ts
+++ quests-engine/src/lib/story.ts
@@ -1,35 +1,45 @@
-import { Graph } from 'graphlib';
 import * as shortID from 'shortid';
 
 export enum NodeTypes {
   Starting = 'Starting'
 }
 
-export function story(graph: Graph, parentID?: string) {
-  return (text: string) => {
-    // if there are zero nodes, we need to set one as a starting point for the quest
-    const ID = graph.nodes().length <= 0 ? NodeTypes.Starting : shortID.generate();
-    graph.setNode(ID, text);
-    if (parentID) {
-      graph.setParent(ID, parentID);
-    }
-    return {
-      choice: choice(graph)(ID),
-      graph
-    };
+export interface StoreNode { 
+  readonly ID: string
+  readonly text: string
+  readonly choices: ReadonlyArray<ChoiceNode>
+}
+
+export interface ChoiceNode {
+  readonly ID: string
+  readonly text: string
+}
+
+export function story(text: string) {
+  // if there are zero nodes, we need to set one as a starting point for the quest
+  const storyNode: StoreNode = {
+    ID: shortID.generate(),
+    choices: [],
+    text
+  }
+  return {
+    choice: choice(storyNode),
+    storyNode
   };
 }
 
-function choice(graph: Graph) {
-  return (parentID: string) => {
-    return (text: string) => {
-      const ID = shortID.generate();
-      graph.setNode(ID, text);
-      graph.setParent(ID, parentID);
-      return {
-        choice: choice(graph)(parentID),
-        graph
-      };
-    };
-  };
+function choice(storyNode: StoreNode) {
+  return (text: string) => {
+    const newStoryNode = {
+      ...storyNode,
+      choices: [...storyNode.choices, {
+        ID: shortID.generate(),
+        text
+      }]
+    }
+    return {
+      choice: choice(newStoryNode),
+      storyNode: newStoryNode
+    }
+  }
 }
