21d3308871cca28b0cbc415ca540853342aa5167
Ali Waseem <waseema393@gmail.com>
2018-08-16 22:07:19 -0600

    added story nodes

--- quests-engine/src/lib/story.spec.ts
+++ quests-engine/src/lib/story.spec.ts
@@ -25,3 +25,18 @@ test('creates a story with a two choice', t => {
   t.is(storyNode.choices[0].text, 'this is a single choice');
   t.is(storyNode.choices[1].text, 'this is the second choice');
 });
+
+test('creates a story with a two choice where one has a story attached', t => {
+  const { storyNode: singleChoiceStory } = story('this is the story for a single choice')
+
+  const { storyNode } = story('this is a test')
+    .choice('this is a single choice',  singleChoiceStory)
+    .choice('this is the second choice');
+
+  t.is(storyNode.choices.length, 2);
+  t.is(storyNode.choices[0].text, 'this is a single choice');
+  t.is(storyNode.choices[1].text, 'this is the second choice');
+
+  const next = storyNode.choices[0].next
+  t.is(next, singleChoiceStory)
+});
